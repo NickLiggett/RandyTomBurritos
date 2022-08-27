cfc738fc7d4d8c6c2ec258ec9901302d1cfb25ec
Nick Liggett <nick.liggett@gmail.com>
2022-08-27 15:05:39 -0600

    Add API Fetch Calls

--- src/Components/MovieDetails/MovieDetails.js
+++ src/Components/MovieDetails/MovieDetails.js
@@ -4,7 +4,7 @@ import "./MovieDetails.css"
 const MovieDetails = ({ movie, viewAll }) => {
     console.log(movie[0].title)
     return (
-        <div className="details-wrapper">
+        <div className="details-wrapper" style={{backgroundImage: `url(${movie[0].backdrop_path})`}}>
             <img className="poster" alt="movie-poster" src={movie[0].poster_path}/>
             <div className="details">
                 <h2>{movie[0].title}</h2>
