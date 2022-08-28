87af433320d35b936dcb6d6b9310d24768f511f0
Abby Luce <abby.c.luce@gmail.com>
2022-08-28 11:26:45 -0600

    Fix css on movie details page

--- src/Components/MovieDetails/MovieDetails.js
+++ src/Components/MovieDetails/MovieDetails.js
@@ -2,15 +2,25 @@ import React from "react"
 import "./MovieDetails.css"
 
 const MovieDetails = ({ movie, viewAll }) => {
-    console.log(movie[0].title)
+
     return (
-        <div className="details-wrapper" style={{backgroundImage: `url(${movie[0].backdrop_path})`}}>
+        <div className="details-wrapper" style={{
+            backgroundImage: `url(${movie[0].backdrop_path})`,
+            // opacity: .5
+            }}>
             <img className="poster" alt="movie-poster" src={movie[0].poster_path}/>
             <div className="details">
-                <h2>{movie[0].title}</h2>
-                <p>Tastiness Rating: {movie[0].average_rating.toFixed(2)}</p>
-                <p>{movie[0].release_date}</p>
-                <button className="view-all-button" onClick={viewAll}>View All Movies</button>
+                <h2>{movie[0].title.toUpperCase()}</h2>
+                <p>
+                    RANDY TOM'S TASTINESS RATING: {movie[0].average_rating.toFixed(2)} 
+                </p> 
+                <p>
+                    RELEASE DATE: {movie[0].release_date}
+                </p>
+                <button className="view-all-button" onClick={viewAll}>RETURN TO ALL MOVIES</button>
+            </div>
+            <div>
+                
             </div>
         </div>
     )
