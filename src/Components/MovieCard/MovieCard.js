1f5f3c6cb8114f694f5250ed46db4657a66edfbf
Nick Liggett <nick.liggett@gmail.com>
2022-08-30 14:19:32 -0600

    Got movie details to populate on refresh.

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -1,22 +1,21 @@
 import React from "react"
 import "./MovieCard.css"
-import { NavLink } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 
 
 const MovieCard = (props) => {   // <---- only pass poster and ID
     return (
-        <NavLink to={`${props.id}`}>
+        <Link to={`${props.id}`}>
         <div className="movie-card" 
             id={props.id}
             style={{
             backgroundImage: `url(${props.posterPath})`, 
             backgroundPosition: "center", 
             backgroundSize: "cover"}}
-            onClick={() => props.fetchSingleMovie(props.id)}
         >
         </div>
-        </NavLink>
+        </Link>
     )
 }
 
