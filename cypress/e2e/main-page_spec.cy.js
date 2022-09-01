8b604834b3eb9edd365817db504219586326e1e6
Abby Luce <abby.c.luce@gmail.com>
2022-09-01 16:18:04 -0600

    Complete cypress testing for back and forward arrows. Add more details to movie details page with formatting

--- cypress/e2e/main-page_spec.cy.js
+++ cypress/e2e/main-page_spec.cy.js
@@ -4,7 +4,7 @@ describe('Main Page', () => {
     cy.visit('http://localhost:3000')
   })
 
-  it('Should render all movies when the user vists the app', () => {
+  it('Should render all movies when the user visits the app', () => {
     cy.contains('Randy Tom\'s Burritos')
       .url().should('include', '/')
       .intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
@@ -24,12 +24,31 @@ describe('Main Page', () => {
   it('Should be able to click on a movie card and display that movie\'s details', () => {
     cy.get('#694919').click()
       .url().should('include', '/694919')
+      .get('.details').should('contain','MONEY PLANE')
+  })
+
+  it('When a movie\'s details are displayed, none of the other movies will be visible', () => {
+    cy.get('#694919').click()
+      .url().should('include', '/694919')
+      .get('.details').should('not.exist','I STILL BELIEVE')
   })
   
   it('Should be able to return to the main page when the user clicks the "return to all movies" button', () => {
     cy.get('#694919').click()
       .get('.view-all-button').click()
       .url().should('include', '/')
+  })
+
+  it('Should be able to click the browser back and forward buttons to navigate between pages', () => {
+    cy.get('#694919').click()
+      .get('.details').should('contain','MONEY PLANE')
+      .go('back')
+      .url().should('include', '/')
+      .get('#585244').should('have.css', 'background-image')
+      .and('include', 'https://image.tmdb.org/t/p/original//dqA2FCzz4OMmXLitKopzf476RVB.jpg')
+      .go('forward')
+      .url().should('include', '/694919')
+      .get('.details').should('contain','MONEY PLANE')
   })
 
 })
